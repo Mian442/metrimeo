2efb83205fdc149f37dce07cd9c06d0fe610fcd5
Mian442 <62568869+Mian442@users.noreply.github.com>
2021-03-19 20:53:46 +0500

    Update HomeScreen.jsx

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,5 +1,6 @@
 import { useNavigation } from "@react-navigation/native";
 import * as React from "react";
+import { ScrollView } from "react-native";
 import { ImageBackground } from "react-native";
 import { StyleSheet, View } from "react-native";
 import { Button, Divider, Text, Title } from "react-native-paper";
@@ -16,68 +17,70 @@ export default function TabOneScreen() {
   // } else
   return (
     <View style={styles.container}>
-      <ImageBackground
-        source={require("../assets/images/Modern-architecture-building.jpg")}
-        style={styles.image}
-      >
-        <View style={{ margin: 20 }}>
-          <Text style={{ marginVertical: 4 }}>
-            <Text style={styles.title}>Welcome to a new</Text>
-          </Text>
+      <ScrollView>
+        <ImageBackground
+          source={require("../assets/images/Modern-architecture-building.jpg")}
+          style={styles.image}
+        >
+          <View style={{ margin: 20 }}>
+            <Text style={{ marginVertical: 4 }}>
+              <Text style={styles.title}>Welcome to a new</Text>
+            </Text>
 
-          <Text style={{ marginVertical: 4 }}>
-            <Text style={styles.title}> World!</Text>
-          </Text>
-          <Text
-            style={[
-              {
-                backgroundColor: "transparent",
-                color: "#FFF",
-                fontSize: 65,
-                marginVertical: 7,
-                fontWeight: "bold",
-                fontFamily: `sans-serif`,
-                textShadowColor: "rgba(0,0,0,0.4)",
-                textShadowOffset: { width: 0, height: 0 },
-                textShadowRadius: 30,
-              },
-            ]}
-          >
-            MetriMeo
-          </Text>
-          <Divider
-            style={{
-              height: 7,
-              marginVertical: 14,
-              marginBottom: 28,
-              backgroundColor: "#e06219",
-              width: "14%",
-            }}
-          />
-          <Text style={{ marginVertical: 4 }}>
-            <Text style={styles.title}>A World of Trust and </Text>
-          </Text>
-          <Text style={{ marginVertical: 4 }}>
-            <Text style={styles.title}>financial progress</Text>
-          </Text>
-          <Button
-            uppercase={false}
-            labelStyle={{ color: "white" }}
-            style={{
-              marginVertical: 28,
-              borderRadius: 25,
-              alignSelf: "flex-start",
-              width: 180,
-              padding: 2,
-            }}
-            labelStyle={{ fontSize: 16 }}
-            mode="contained"
-            onPress={() => navigation.navigate("Profile")}
-          >
-            Get Started!
-          </Button>
-        </View>
-      </ImageBackground>
+            <Text style={{ marginVertical: 4 }}>
+              <Text style={styles.title}> World!</Text>
+            </Text>
+            <Text
+              style={[
+                {
+                  backgroundColor: "transparent",
+                  color: "#FFF",
+                  fontSize: 65,
+                  marginVertical: 7,
+                  fontWeight: "bold",
+                  fontFamily: `sans-serif`,
+                  textShadowColor: "rgba(0,0,0,0.4)",
+                  textShadowOffset: { width: 0, height: 0 },
+                  textShadowRadius: 30,
+                },
+              ]}
+            >
+              MetriMeo
+            </Text>
+            <Divider
+              style={{
+                height: 7,
+                marginVertical: 14,
+                marginBottom: 28,
+                backgroundColor: "#e06219",
+                width: "14%",
+              }}
+            />
+            <Text style={{ marginVertical: 4 }}>
+              <Text style={styles.title}>A World of Trust and </Text>
+            </Text>
+            <Text style={{ marginVertical: 4 }}>
+              <Text style={styles.title}>financial progress</Text>
+            </Text>
+            <Button
+              uppercase={false}
+              labelStyle={{ color: "white" }}
+              style={{
+                marginVertical: 28,
+                borderRadius: 25,
+                alignSelf: "flex-start",
+                width: 180,
+                padding: 2,
+              }}
+              labelStyle={{ fontSize: 16 }}
+              mode="contained"
+              onPress={() => navigation.navigate("Profile")}
+            >
+              Get Started!
+            </Button>
+          </View>
+        </ImageBackground>
+      </ScrollView>
     </View>
   );
 }
